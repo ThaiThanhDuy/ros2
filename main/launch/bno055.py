fb1625d3ffa23d1fa07707829f44ae2fc2cb08b0
Duy <108132255+ThaiThanhDuy@users.noreply.github.com>
2024-12-19 19:24:12 +0700

    Update bno055.py

--- main/launch/bno055.py
+++ main/launch/bno055.py
@@ -6,7 +6,12 @@ BNO055_ADDRESS = 0x28
 
 # BNO055 register addresses
 BNO055_REG_CHIP_ID = 0x00
+BNO055_REG_OPR_MODE = 0x3D
+BNO055_REG_SYS_TRIGGER = 0x3F
 BNO055_REG_EULER_H = 0x1A  # Start of Euler angles
+
+# Operation modes
+OPR_MODE_NDOF = 0x0C  # NDOF mode
 
 # Initialize I2C bus
 bus = smbus.SMBus(1)  # Use 1 for Raspberry Pi 2 and later
@@ -14,6 +19,10 @@ bus = smbus.SMBus(1)  # Use 1 for Raspberry Pi 2 and later
 # Function to read a byte from a register
 def read_byte(register):
     return bus.read_byte_data(BNO055_ADDRESS, register)
+
+# Function to write a byte to a register
+def write_byte(register, value):
+    bus.write_byte_data(BNO055_ADDRESS, register, value)
 
 # Function to read 6 bytes of data (Euler angles)
 def read_euler():
@@ -28,6 +37,14 @@ try:
     # Check the chip ID
     chip_id = read_byte(BNO055_REG_CHIP_ID)
     print(f"BNO055 Chip ID: {chip_id}")
+
+    # Reset the sensor
+    write_byte(BNO055_REG_SYS_TRIGGER, 0x20)
+    time.sleep(0.5)
+
+    # Set the operation mode to NDOF
+    write_byte(BNO055_REG_OPR_MODE, OPR_MODE_NDOF)
+    time.sleep(0.5)
 
     while True:
         heading, roll, pitch = read_euler()
